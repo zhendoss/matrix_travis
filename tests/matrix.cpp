64ac405abaf9719235b9d3c590337f18bdf6af94
Levina Svetlana <36290990+Vampirchik147@users.noreply.github.com>
2018-04-21 19:27:48 +0300

    Update matrix.cpp

--- tests/matrix.cpp
+++ tests/matrix.cpp
@@ -1,11 +1,27 @@
 #include "matrix.hpp"
 #include "catch.hpp"
 
-TEST_CASE("test matrix","[matrix]"){
-    string A = "A.txt";
+TEST_CASE("creat"){
     Matrix matrix;
-    matrix.Read(A);
-    REQUIRE(matrix.Rows(A) == 3);
-    REQUIRE(matrix.Columns(A) == 3);
-    REQUIRE(3 == 3);
+    REQUIRE(matrix.rows() == 0);
+    REQUIRE(matrix.rolumns() == 0);
+}
+
+TEST_CASE("read"){
+    std::string input{
+        "3, 3\n"
+        "1 1 1\n"
+        "2 2 2\n"
+        "3 3 3" };
+    matrix_t matrix;
+    std::istringstream istream{ input };
+    
+    REQUIRE( matrix.read( istream ) );
+    REQUIRE( matrix.rows() == 3 );
+    REQUIRE( matrix.collumns() == 3 );
+    
+    std::ostringstream ostream;
+    matrix.write( ostream );
+    
+    REQUIRE( input == ostream.str() );
 }
