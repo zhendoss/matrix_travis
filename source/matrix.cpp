93dfb498d751eca9dcb4effb0bebf4cede9f5fcd
Levina Svetlana <36290990+Vampirchik147@users.noreply.github.com>
2018-04-21 19:35:24 +0300

    Update matrix.cpp

--- source/matrix.cpp
+++ source/matrix.cpp
@@ -3,71 +3,29 @@
 
 using namespace std;
 
-Matrix::Matrix(int row, int cols):  rows(row), columns(cols) {
-    arr = new int *[row];
-    for (int i = 0; i < row; i++) {
-        arr[i] = new int[cols]{};
-    }
+Matrix::Matrix() : arr{ nullptr }, rows{ 0 }, columns{ 0 }
+{
 }
 
-Matrix::~Matrix() {
-    for(int i = 0; i < rows; i++){
-            delete[] arr[i];
-        }
-        delete[] arr;
+Matrix::Matrix( Matrix const & other )
+{
 }
 
-int Matrix::Rows(string filename) {
-    ifstream file(filename);
-    int row;
-    if (!file.is_open()){
-        cout << "ERROR";
-    }else{
-        file >> row;
-    }
-    file.close();
-    return row;
+Matrix & Matrix::operator =( Matrix const & other )
+{
+	return *this;
 }
 
-int Matrix::Columns(string filename) {
-     ifstream file(filename);
-    int row, column; 
-    if (!file.is_open()){
-        cout << "ERROR";
-    }else{
-        file >> row>>column;
-    }
-    file.close();
-return column;
+Matrix::~Matrix()
+{
 }
 
-void Matrix::Read(string filename) {
-    ifstream file(filename);
-//
-        if (!file.is_open()){
-            cout << "ERROR";
-        }else{
-            file >> rows >> columns;
-
-            arr = new int*[rows];
-            for(int i = 0; i < rows; i++){
-                arr[i] = new int[columns];
-            }
-
-            for(int i = 0; i < rows; i++){
-                for(int j = 0; j < columns; j++){
-                    file >> arr[i][j];
-                }
-            }
-        }
-        file.close();
+int Matrix::row() const
+{
+    return rows;
 }
 
-void Matrix::Print() {
-    for(int i = 0; i < rows; i++){
-        for(int j = 0; j < columns; j++){
-            cout << arr[i][j] << " ";
-        }
-        cout<<endl;
-    }
+int Matrix::column() const
+{
+    return columns;
 }
