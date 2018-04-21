ceb5c03662dac27e3ce61194b61d5c6475cab1cd
Levina Svetlana <36290990+Vampirchik147@users.noreply.github.com>
2018-04-21 19:39:36 +0300

    Update matrix.cpp

--- source/matrix.cpp
+++ source/matrix.cpp
@@ -29,3 +29,66 @@ int Matrix::column() const
 {
     return columns;
 }
+
+istream & Matrix::read( std::istream & stream )
+{
+    int r;
+    int c;
+    char symbol;
+    
+    bool success = true;
+    if( stream >> r && stream >> symbol && symbol == ',' && stream >> c ) {
+        int ** a = new int *[ r ];
+        for( int i = 0; success && i < r; ++i ) {
+            a[ i ] = new int[ c ];
+            for( int j = 0; j < c; ++j ) {
+                if( !( stream >> a[ i ][ j ] ) ) {
+                    success = false;
+                    break;
+                }
+            }
+        }
+        
+        if( success ) {
+            for( int i = 0; i < rows; ++i ) {
+                delete [] arr[ i ];
+            }
+            delete [] arr;
+            
+            rows = r;
+            columns = c;
+            arr = a;
+        }
+        else {
+            for( int i = 0; i < r; ++i ) {
+                delete [] a[ i ];
+            }
+            delete [] a;
+        }
+    }
+    else {
+        success = false;
+    }
+    
+    if( !success ) {
+        stream.setstate(ios_base::failbit );
+    }
+    
+	return stream;
+}
+
+ostream & Matrix::write(ostream & stream ) const
+{
+    stream << rows << ", " << collumns;
+    for( int i = 0; i < rows; ++i ) {
+        stream << '\n';
+        for( int j = 0; j < collumns; ++j ) {
+            stream << arr[ i ][ j ];
+            if( j != rows - 1 ) {
+                stream << ' ';
+            }
+        }
+    }
+    
+	return stream;
+}
