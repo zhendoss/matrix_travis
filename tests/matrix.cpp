0ffd2502d6c1e6b7e84508f8091d5f7d45649cd6
Levina Svetlana <36290990+Vampirchik147@users.noreply.github.com>
2018-04-21 18:32:20 +0300

    Update matrix.cpp

--- tests/matrix.cpp
+++ tests/matrix.cpp
@@ -5,7 +5,7 @@ TEST_CASE("test matrix","[matrix]"){
     string A = "/file/A.txt";
     Matrix matrix;
     matrix.Read(A);
-    REQUIRE(matrix.Rows() == 3);
-    REQUIRE(matrix.Columns() == 3);
+    REQUIRE(matrix.Rows(A) == 3);
+    REQUIRE(matrix.Columns(A) == 3);
     REQUIRE(3 == 3);
 }
